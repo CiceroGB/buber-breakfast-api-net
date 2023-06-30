fd753e644dc18676edd273898d69328f2eeef838
CiceroGB <brunelli.cg@gmail.com>
2023-06-30 00:54:29 -0300

    feat: controller without route explicit

--- BuberBreakfast/Controllers/BreackfastsController.cs
+++ BuberBreakfast/Controllers/BreackfastsController.cs
@@ -5,32 +5,33 @@ namespace BuberBreakfast.Controllers;
 
 
 [ApiController]
-public class BreakfastController : ControllerBase
+[Route("[controller]")]
+public class BreakfastsController : ControllerBase
 {
-    [HttpPost("/breakfasts")]
+    [HttpPost()]
     public IActionResult CreateBreakfast(CreateBreakfastRequest request)
     {
         return Ok(request);
     }
 
-    [HttpGet("/breakfasts/{id:guid}")]
+    [HttpGet("{id:guid}")]
     public IActionResult GetBreakfast(Guid id)
     {
-        var response = new { msg = "ok"};
+        var response = new { msg = "ok" };
         return Ok(response);
-}
+    }
 
-[HttpPut("/breakfasts/{id:guid}")]
-public IActionResult UpdateBreakfast(Guid id, UpsertBreakfastRequest request)
-{
-    return Ok(request);
-}
+    [HttpPut("{id:guid}")]
+    public IActionResult UpdateBreakfast(Guid id, UpsertBreakfastRequest request)
+    {
+        return Ok(request);
+    }
 
-[HttpDelete("/breakfasts/{id:guid}")]
-public IActionResult DeleteBreakfast(Guid id)
-{
-    return Ok(id);
-}
+    [HttpDelete("{id:guid}")]
+    public IActionResult DeleteBreakfast(Guid id)
+    {
+        return Ok(id);
+    }
 
 
 }
